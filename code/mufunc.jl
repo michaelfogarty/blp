1ca617430a6e6a5a742e0258f9f40f799de6c426
michaelfogarty <mfogs1@gmail.com>
2022-10-22 08:33:14 -0500

    Add mufunc to calculate nonlinear portion of utility

---
+++ code/mufunc.jl
@@ -0,0 +1,16 @@
+function mufunc(theta2w::Array{Float64,2}, res::Results, data::Data)
+    # compute nonlinear portion of utility (μ_ijt)
+    @unapck x2, ns, vfull, dfull = data
+
+    n,k = size(x2, 1), size(x2,2);
+    j = size(theta2w,2)-1;
+    μ = zeros(n, ns);
+
+    for i in 1:ns
+        v_i = vfull[:,i:ns:k*ns];
+        d_i = dfull[:,i:ns:j*ns];
+        μ[:,i] = (x2 .* v_i * theta2w[:,1]) + x2 .* (d_i * theta2w[:,2:j+1]') * ones(k,1);
+    end
+
+    return μ
+end
